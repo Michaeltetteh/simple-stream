6564b3ca49546b71b0eedb683805d5e3422508e5
mike <miketetteh751@outlook.com>
2024-09-14 17:35:19 +0000

    admin controllers

--- exstreamer/lib/exstreamer_web/router.ex
+++ exstreamer/lib/exstreamer_web/router.ex
@@ -65,6 +65,8 @@ defmodule ExstreamerWeb.Router do
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+ 
+    resources "/movies/", MoviesController, only: [:create, :edit, :index, :update, :show]
   end
 
   scope "/admin", ExstreamerWeb do
